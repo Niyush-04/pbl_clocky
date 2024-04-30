c60fc0356b525152347cd3bf6c626429acc70838
Niyush-04 <mauryapradip04@gmail.com>
2024-04-30 21:00:23 +0530

    fixing pomodoro timer launching twice on double click

--- app/src/main/java/itm/pbl/clocky/presentation/pomodoro/PomodoroViewModel.kt
+++ app/src/main/java/itm/pbl/clocky/presentation/pomodoro/PomodoroViewModel.kt
@@ -3,7 +3,7 @@ package itm.pbl.clocky.presentation.pomodoro
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,34 +13,33 @@ import kotlinx.coroutines.launch
 class PomodoroViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(PomodoroUiState())
     val uiState: StateFlow<PomodoroUiState> = _uiState.asStateFlow()
-
-    private var coroutineScope = CoroutineScope(Dispatchers.Main)
+    private var timerJob: Job? = null
 
     fun startTimer(selectedMinute: Int) {
-
+        if (timerJob?.isActive != true) {
             _uiState.value = _uiState.value.copy(
-                totalTime = selectedMinute * 60 * 1000L,
-                timeLeft = selectedMinute * 60 * 1000L
+                totalTime = selectedMinute * 60 * 1000L, timeLeft = selectedMinute * 60 * 1000L
             )
 
-        coroutineScope.launch {
-            while (_uiState.value.timeLeft > 0) {
-                delay(1000L)
-                val timeLeft = _uiState.value.timeLeft - 1000L
-                val updateMinutes = String.format("%02d", (timeLeft / 1000 / 60) % 60)
-                val updateSeconds = String.format("%02d", (timeLeft / 1000) % 60)
-                _uiState.value = _uiState.value.copy(
-                    timeLeft = timeLeft,
-                    minutes = updateMinutes,
-                    seconds = updateSeconds,
-                    isPaused = false
-                )
+            timerJob = CoroutineScope(Dispatchers.Main).launch {
+                while (_uiState.value.timeLeft > 0) {
+                    delay(1000L)
+                    val timeLeft = _uiState.value.timeLeft - 1000L
+                    val updateMinutes = String.format("%02d", (timeLeft / 1000 / 60) % 60)
+                    val updateSeconds = String.format("%02d", (timeLeft / 1000) % 60)
+                    _uiState.value = _uiState.value.copy(
+                        timeLeft = timeLeft,
+                        minutes = updateMinutes,
+                        seconds = updateSeconds,
+                        isPaused = false
+                    )
+                }
             }
         }
     }
+
     fun resetTimer() {
-        coroutineScope.cancel()
-        coroutineScope = CoroutineScope(Dispatchers.Main)
+        timerJob?.cancel()
         _uiState.value = PomodoroUiState()
     }
 }
