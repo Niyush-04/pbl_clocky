54a8ce52c9cce3f846f1e8b6f5aac84190bb56ac
Niyush-04 <mauryapradip04@gmail.com>
2024-05-01 18:45:05 +0530

    updating version

--- app/src/main/java/itm/pbl/clocky/data/AlarmDatabase.kt
+++ app/src/main/java/itm/pbl/clocky/data/AlarmDatabase.kt
@@ -5,7 +5,8 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Alarm::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class AlarmDatabase : RoomDatabase() {
     abstract val dao: AlarmDao
